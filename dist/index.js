c7e6b14f248c2656b8f049baafb7a3fffc758065
William Overton <woverton@fastly.com>
2021-10-29 16:45:48 +0100

    Cleanup for module

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-import { Router } from "./lib/router";
-export { Router };
--- dist/lib/middleware.js
+++
@@ -1,9 +0,0 @@
-export default class Middleware {
-    constructor(callback) {
-        this.callback = callback;
-    }
-    async run(req, res) {
-        // Supply an empty callback which would normally be next() in express
-        await this.callback(req, res, () => { });
-    }
-}
