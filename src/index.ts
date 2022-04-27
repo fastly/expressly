357cbf73802b6d4ec50eac50031ff74aaa0ee422
doramatadora <hello@doramilitaru.com>
2022-04-27 10:53:23 +0100

    retire fetcher

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "./lib/routing/router";
-import { Fetcher } from "./lib/utilities/fetcher";
 import FPRequest from "./lib/routing/request";
 import FPResponse from "./lib/routing/response";
 import { MiddlewareCallback, Middleware } from "./lib/routing/middleware";
 
 export {
   Router,
-  Fetcher,
   FPRequest,
   FPResponse,
   MiddlewareCallback,
