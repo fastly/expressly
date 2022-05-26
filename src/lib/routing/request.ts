ed59c919459dffecb3ea8f5255db9983fd4afaa9
doramatadora <hello@doramilitaru.com>
2022-05-26 15:22:06 +0100

    refactor cookie, header and querystring behaviour

--- src/lib/routing/request.ts
+++ src/lib/routing/request.ts
@@ -2,58 +2,39 @@ import cookie from "cookie";
 
 export default class FPRequest {
   readonly clientInfo: {} = {};
-  private _headers: Headers;
+  _headers: Headers;
   readonly method: string;
   url: URL;
   params: { [key: string]: string } = {};
-  query: { [key: string]: string };
-  private _cookies: {} = {};
-  cookies: any = {};
+  query: URLSearchParams;
+  cookies: Map<string, string> = new Map();
 
   constructor(private config: any, private event: FetchEvent) {
     this.clientInfo = event.client;
     this._headers = event.request.headers;
     this.method = event.request.method;
     this.url = new URL(event.request.url);
-    this.query = Object.fromEntries(this.url.searchParams.entries());
+    this.query = this.url.searchParams;
 
-    if(config.parseCookies && this._headers.has("cookie")){
-      this._cookies = cookie.parse(this._headers.get("cookie"));
-    }
-
-    // If a cookie is set on the Request object, we need to re-serialize the `cookie` header
-    // in case the request is forwarded to an origin
-    this.cookies = new Proxy(this._cookies, {
-      get: (target, key) => {
-        return target[key];
-      },
-      set: (target, key, value) => {
-        target[key] = value;
-        this.reSerializeCookies();
-        return true;
+    if (config.parseCookies && this._headers.has("cookie")) {
+      for (const [key, value] of Object.entries(cookie.parse(this._headers.get("cookie") || ""))) {
+        if (typeof value === "string") {
+          this.cookies.set(key, value);
+        }
       }
-    });
-  }
-
-  private reSerializeCookies() {
-    console.log("reSerializeCookies")
-    this._headers.set("cookie", Object.entries(this._cookies).map(([key, value]) => `${key}=${value}`).join("; "));
+    }
   }
 
   get headers() {
-    return Object.fromEntries(this._headers.entries())
-  }
-
-  setHeader(key: string, value: string): void {
-    this._headers.set(key, value);
-  }
-
-  appendHeader(key: string, value: string): void {
-    this._headers.append(key, value);
-  }
-
-  removeHeader(key: string): void {
-    this._headers.delete(key);
+    if (this.config.parseCookies && this.cookies.size) {
+      // Serialize cookies
+      const cookieArray = [];
+      for (const [key, value] of this.cookies.entries()) {
+        cookieArray.push(`${key}=${value}`);
+      }
+      this._headers.set("cookie", cookieArray.join("; "));
+    }
+    return this._headers;
   }
 
   async json() {
