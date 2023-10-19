700c18fdc2b154697d71cbcedc8066a44fa61175
Jake Champion <jchampion@fastly.com>
2023-10-19 15:08:36 +0100

    🧑‍🔬 Allow programs to extend the EReq and ERes types used within the Router and support type inference in the Router methods
    Previously typescript could not infer the types used in the callbacks on the Router methods `use`, `get` etc
    
    This patch brings supports for typescript to infer types in the callbacks correctly and also brings support for applications to extend those types.
    
    What this means is we can now implement applications which add new methods to the Request or Response, and have those new methods be understood by our type signatures.
    
    E.G - this is now all type-checked correctly:
    ```ts
    interface MeowReq extends EReq {
        meow(): string
      }
    
      interface WoofReq extends EReq {
        meow(): string
      }
    
      interface MyReq extends MeowReq, WoofReq { }
    
      interface WoofRes extends ERes {
        woof(): string
      }
    
      const router = new Router<MyReq, WoofRes>();
      router.all('/', async (req, res) => {
        req.meow()
        res.woof()
        return res.send("Hello world!");
      });
    ```

---
+++ test-d/router-test-d.ts
@@ -0,0 +1,51 @@
+import { expectType } from 'tsd';
+import { Router } from "../dist";
+import { EReq } from "../dist/lib/routing/request";
+import { ERes } from '../dist/lib/routing/response';
+
+// Default Req and Res types
+{
+  const router = new Router();
+  router.all('/', async (req, res) => {
+    expectType<EReq>(req)
+    expectType<ERes>(res)
+    return res.send("Hello world!");
+  });
+  router.get('/', async (req, res) => {
+    expectType<EReq>(req)
+    expectType<ERes>(res)
+    return res.send("Hello world!");
+  });
+}
+
+// Custom Req and Res types
+{
+  interface MeowReq extends EReq {
+    meow(): string
+  }
+
+  interface WoofReq extends EReq {
+    woof(): string
+  }
+
+  interface MyReq extends MeowReq, WoofReq { }
+
+  interface WoofRes extends ERes {
+    woof(): string
+  }
+
+  const router = new Router<MyReq, WoofRes>();
+  router.all('/', async (req, res) => {
+    expectType<MyReq>(req)
+    expectType<WoofRes>(res)
+    req.meow()
+    req.woof()
+    res.woof()
+    return res.send("Hello world!");
+  });
+  router.get('/', async (req, res) => {
+    expectType<MyReq>(req)
+    expectType<WoofRes>(res)
+    return res.send("Hello world!");
+  });
+}
